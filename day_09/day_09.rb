762915b253263a933816060db33b8a057374004d
Alex <alexandre.klein@tanukisoftware.com>
2015-12-09 18:27:50 +0900

    add day 9

---
+++ day_09/day_09.rb
@@ -0,0 +1,76 @@
+#!/usr/bin/env ruby
+
+puts "--- Day 9: All in a Single Night ---"
+
+$cities = {}
+$min_distance = 9999
+$max_distance = 0
+
+# Recursive function
+def travel(city, visited, dist)
+    
+    if visited.include?(city)
+        # we can't visit twice the same city
+        return
+    end
+    
+    visited.push(city)
+    
+    if visited.length == $max_city
+        # we visited all the cities
+        if dist < $min_distance
+            $min_distance = dist
+        end
+        
+        if dist > $max_distance
+            $max_distance = dist
+        end
+        
+        visited.pop
+        return
+    end
+    
+    links = $cities[city]
+    
+    links.each do |to, km|
+        travel(to, visited, dist+km.to_i)
+    end
+    
+    visited.pop
+end
+
+File.open("input.txt", "r") do |f|
+    f.each_line do |route|
+        route.chomp!
+        
+        from, to, distance = route.match(/^(\w+) to (\w+) = (\d+)$/).captures
+        
+        unless $cities.has_key?(from)
+            $cities[from] = Hash.new
+        end
+        
+        unless $cities.has_key?(to)
+            $cities[to] = Hash.new
+        end
+           
+        $cities[from][to] = distance
+        $cities[to][from] = distance
+        
+    end
+end
+
+$max_city = $cities.keys.length
+
+visited = Array.new
+
+# Note: I should have use $cities.keys.permutation
+$cities.each do |city, links|
+    travel(city, visited, 0)
+end
+
+
+puts "--- Part One ---"
+puts "Min distance is: #{$min_distance}"
+
+puts "--- Part Two ---"
+puts "Max distance is: #{$max_distance}"
